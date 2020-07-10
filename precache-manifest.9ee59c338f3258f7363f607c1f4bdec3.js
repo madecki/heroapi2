98f312be05e3749c2842737ee6a50a5681eadec7
Radosław <radoslaw.madecki@gmail.com>
2020-07-10 08:02:42 +0200

    Updates

---
+++ precache-manifest.9ee59c338f3258f7363f607c1f4bdec3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31207eb51beaf08fc1029593110ef116",
+    "url": "/heroapi2/index.html"
+  },
+  {
+    "revision": "2c9bd55a506734930d0e",
+    "url": "/heroapi2/static/css/main.b881a246.chunk.css"
+  },
+  {
+    "revision": "649690f6c5e2de66c6e5",
+    "url": "/heroapi2/static/js/2.33aa280f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/heroapi2/static/js/2.33aa280f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c9bd55a506734930d0e",
+    "url": "/heroapi2/static/js/main.1306e048.chunk.js"
+  },
+  {
+    "revision": "dd5b6e38345e9fd5a238",
+    "url": "/heroapi2/static/js/runtime-main.da816e44.js"
+  },
+  {
+    "revision": "1698dbf22f1edd1b93bb1d2fb73a5024",
+    "url": "/heroapi2/static/media/boxing.1698dbf2.svg"
+  },
+  {
+    "revision": "68a9807aaf574e103a9d5e4c42eae83b",
+    "url": "/heroapi2/static/media/durable.68a9807a.svg"
+  },
+  {
+    "revision": "90c5025d7cfd4d283921a6b45f39b585",
+    "url": "/heroapi2/static/media/fist.90c5025d.svg"
+  },
+  {
+    "revision": "711aa06f30b5e1e8b26aa1835e67a3da",
+    "url": "/heroapi2/static/media/nav_logo.711aa06f.png"
+  },
+  {
+    "revision": "f09b4e842c927e2ba5ca621d932df4f1",
+    "url": "/heroapi2/static/media/speedometer.f09b4e84.svg"
+  },
+  {
+    "revision": "e11713e8a33bf31f8b4af671be4a3826",
+    "url": "/heroapi2/static/media/thinking.e11713e8.svg"
+  }
+]);
